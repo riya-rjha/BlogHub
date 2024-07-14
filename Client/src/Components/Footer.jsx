c7b03b4cf25a9141ef6fd0d3fbed4c5ee5684434
RRJ <riyaranjanjha2006@gmail.com>
2024-07-14 11:33:11 +0530

    Updated UI of Footer

--- Client/src/Components/Footer.jsx
+++ Client/src/Components/Footer.jsx
@@ -4,58 +4,62 @@ import { SiHashnode } from "react-icons/si";
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="container mx-auto">
-        <div className="flex flex-col md:flex-row justify-between items-center">
-          <div className="flex-1 text-center md:text-left mb-4 md:mb-0">
+    <footer className="bg-gray-900 text-gray-400">
+      <div className="container p-4  mx-auto">
+        <div className="flex flex-col lg:flex-row items-center justify-between">
+          <div className="mb-8 lg:mb-0">
             <h3 className="text-2xl font-bold">Blog Application</h3>
-            <p className="text-gray-400 mt-3">Made by Riya Ranjan Jha</p>
+            <p className="mt-2">
+              Made by <span className="text-white font-semibold"> Riya Ranjan Jha</span>
+            </p>
           </div>
-          <div className="flex-1 flex justify-center md:justify-center mb-4 md:mb-0">
+          <div className="flex items-center mb-4 lg:mb-0">
             <a
+              href="https://www.linkedin.com/in/riya-ranjan-jha-751688249/"
               target="_blank"
               rel="noopener noreferrer"
-              href="https://www.linkedin.com/in/riya-ranjan-jha-751688249/"
-              className="mx-2 text-gray-400 hover:text-blue-500 transition delay-75"
+              className="text-gray-400 hover:text-blue-500 transition duration-300 ease-in-out"
             >
               <FaLinkedin size={30} />
             </a>
             <a
+              href="https://x.com/RiyaJha25892"
               target="_blank"
               rel="noopener noreferrer"
-              href="https://x.com/RiyaJha25892"
-              className="mx-2 text-gray-400 hover:text-blue-800 transition delay-75"
+              className="ml-4 text-gray-400 hover:text-blue-500 transition duration-300 ease-in-out"
             >
               <FaTwitter size={30} />
             </a>
             <a
+              href="https://riyarjha.vercel.app/"
               target="_blank"
               rel="noopener noreferrer"
-              href="https://riyarjha.vercel.app/"
-              className="mx-2 text-gray-400 hover:text-pink-400 transition delay-75"
+              className="ml-4 text-gray-400 hover:text-pink-400 transition duration-300 ease-in-out"
             >
               <SiHashnode size={30} />
             </a>
           </div>
-          <div className="flex-1 text-center md:text-right">
-            <a
-              href="/about"
-              className="text-gray-400 hover:text-white mx-2 cursor-default"
-            >
-              ReactJS
-            </a>
-            <a
-              href="/contact"
-              className="text-gray-400 hover:text-white mx-2 cursor-default"
-            >
-              MongoDB
-            </a>
-            <a
-              href="/privacy"
-              className="text-gray-400 hover:text-white mx-2 cursor-default"
-            >
-              NodeJS
-            </a>
+          <div className="mt-8 lg:mt-0">
+            <nav className="flex flex-wrap justify-center lg:justify-end -mx-2">
+              <a
+                href="/about"
+                className="mx-2 text-gray-400 hover:text-white transition duration-300 ease-in-out cursor-default"
+              >
+                ReactJS
+              </a>
+              <a
+                href="/contact"
+                className="mx-2 text-gray-400 hover:text-white transition duration-300 ease-in-out cursor-default"
+              >
+                MongoDB
+              </a>
+              <a
+                href="/privacy"
+                className="mx-2 text-gray-400 hover:text-white transition duration-300 ease-in-out cursor-default"
+              >
+                NodeJS
+              </a>
+            </nav>
           </div>
         </div>
       </div>
