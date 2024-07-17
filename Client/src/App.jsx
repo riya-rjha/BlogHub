29ef11691b8bbd829994ceee6088c7729b23a4f8
RRJ <riyaranjanjha2006@gmail.com>
2024-07-17 12:24:38 +0530

    Added top arrow button

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -7,12 +7,18 @@ import Single from "./Pages/Single";
 import Register from "./Pages/Register";
 import Login from "./Pages/Login";
 import Write from "./Pages/Write";
-import EditBlog from './Pages/EditBlog';
+import EditBlog from "./Pages/EditBlog";
 import PageNotFound from "./Components/PageNotFound";
 
 const App = () => {
   return (
-    <div id="app" className="relative z-10 min-h-full">
+    <div id="top" className="relative z-10 min-h-full">
+      <a href="#top">
+        <img
+          src="https://cdn-icons-png.freepik.com/256/15992/15992789.png?ga=GA1.1.224769648.1717002388&semt=ais_hybrid"
+          className="fixed w-16 bottom-5 right-5 z-50"
+        />
+      </a>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
@@ -20,8 +26,8 @@ const App = () => {
         <Route path="/write" element={<Write />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/register" element={<Register />}></Route>
-        <Route path='/edit/:id' element={<EditBlog/>}></Route>
-        <Route path='*' element={<PageNotFound/>}></Route>
+        <Route path="/edit/:id" element={<EditBlog />}></Route>
+        <Route path="*" element={<PageNotFound />}></Route>
       </Routes>
       <Footer />
     </div>
