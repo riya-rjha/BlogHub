07ec0d3a3835575fafeb732d826d7854448f13f6
RRJ <riyaranjanjha2006@gmail.com>
2024-07-19 23:42:18 +0530

    chore: Set userId to null on logout
    ```

--- Client/src/Context/authContext.jsx
+++ Client/src/Context/authContext.jsx
@@ -35,6 +35,7 @@ export const AuthContextProvider = ({ children }) => {
       }
     );
     setCurrentUser(null);
+    setUserId(null);
     localStorage.removeItem("token");
     localStorage.removeItem("userId");
   };
