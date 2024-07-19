96e1fd153182a202fde005da25756cedb255571a
RRJ <riyaranjanjha2006@gmail.com>
2024-07-19 23:39:41 +0530

    Update authContext.jsx to use arrow functions for localStorage retrieval

--- Client/src/Context/authContext.jsx
+++ Client/src/Context/authContext.jsx
@@ -6,10 +6,10 @@ export const AuthorizationContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(
-    JSON.parse(localStorage.getItem("user")) || null
+    () => JSON.parse(localStorage.getItem("user")) || null
   );
   const [userId, setUserId] = useState(
-    JSON.parse(localStorage.getItem("userId")) || null
+    () => JSON.parse(localStorage.getItem("userId")) || null
   );
 
   const login = async (inputs) => {
