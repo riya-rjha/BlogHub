b3c9b3a253e7bc465d226f3b5960bf7ce64aaf59
RRJ <riyaranjanjha2006@gmail.com>
2024-07-21 15:09:09 +0530

    feat: Update post route to include file upload functionality

--- Server/Routes/post.js
+++ Server/Routes/post.js
@@ -41,7 +41,7 @@ postRouter.post("/upload", upload.single("file"), function (req, res) {
 });
 
 // Post a blog
-postRouter.post("/", async (req, res) => {
+postRouter.post("/", upload.single("file"), async (req, res) => {
   const token = req.cookies.access_token;
   if (!token) return res.status(401).json({ error: "Not authenticated" });
   try {
